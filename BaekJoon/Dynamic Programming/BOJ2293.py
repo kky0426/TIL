870f279217d131d0212c640f1260df133fb88fbe
kky0426 <ksakdlf99@naver.com>
2021-01-25 03:39:54 +0900

    동전 1

---
+++ BaekJoon/Dynamic Programming/BOJ2293.py
@@ -0,0 +1,15 @@
+n,k=map(int,input().split())
+
+coin=[]
+
+for i in range(n):
+    coin.append(int(input()))
+
+dp=[0]*(k+1)
+dp[0]=1
+
+for i in coin:
+    for j in range(i,k+1):
+        dp[j]+=dp[j-i]
+
+print(dp[k])
