0cdaf42204a9b2bc0fdc339035a0880fa3b3d22a
kky0426 <ksakdlf99@naver.com>
2021-01-29 18:04:25 +0900

    스택

---
+++ BaekJoon/Data Structure/BOJ10828.py
@@ -0,0 +1,25 @@
+import sys
+n=int(input())
+
+stack=[]
+for i in range(n):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
