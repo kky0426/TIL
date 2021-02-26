bd66d7b33f4c8503efd1c7c08b71a6246aa9f475
kky0426 <ksakdlf99@naver.com>
2021-02-26 21:27:36 +0900

    오르막 수

---
+++ BaekJoon/Dynamic Programming/BOJ11057.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp=[[0]*10 for _ in range(1001)]
+
+for i in range(10):
+    dp[1][i]=1
+
+for i in range(2,1001):
+    for j in range(10):
+        for k in range(j+1):
+            dp[i][j] += dp[i-1][k]
+
+print(sum(dp[N])%10007)
