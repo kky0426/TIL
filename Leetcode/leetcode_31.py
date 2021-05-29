cdcd4fda6d74580db860546151fdb3f26fe2d257
kky0426 <ksakdlf99@naver.com>
2021-05-29 23:53:00 +0900

    commit

---
+++ Leetcode/leetcode_31.py
@@ -0,0 +1,28 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i=len(nums)-2
+        while i>=0:
+            if nums[i]<nums[i+1]:
+                break
+            i-=1
+        
+        if i>=0:
+            j=len(nums)-1
+            while nums[j]<=nums[i]:
+                j-=1
+
+                
+            nums[i],nums[j]=nums[j],nums[i]
+            start=i+1
+            end=len(nums)-1
+            while start<end:
+                nums[start],nums[end]=nums[end],nums[start]
+                start+=1
+                end-=1
+            
+        else:
+            nums.sort()
+       
