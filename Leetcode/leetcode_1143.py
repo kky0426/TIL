a3e36841428994c77427a171d774105df6325124
kky0426 <ksakdlf99@naver.com>
2021-06-06 17:57:17 +0900

    commit

---
+++ Leetcode/leetcode_1143.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        N=len(text1)
+        M=len(text2)
+        dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
+        
+        for i in range(1,N+1):
+            for j in range(1,M+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j])
+        return dp[-1][-1]
---
+++ Leetcode/leetcode_583.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        N=len(word1)
+        M=len(word2)
+        dp=[[0 for _ in range(M+1)] for _ in range(N+1)]
+        
+        for i in range(1,N+1):
+            for j in range(1,M+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:  
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        
+        return N+M-2*dp[-1][-1]
