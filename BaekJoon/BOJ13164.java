f33507567a65478e58c891a3cbcbb243d7d54369
kky0426 <ksakdlf99@naver.com>
2021-10-24 17:32:12 +0900

    commit

---
+++ BaekJoon/BOJ13164.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] nums = new int[N];
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] diff = new int[N-1];
+        for(int i=0; i<N-1; i++){
+            diff[i] = nums[i+1]-nums[i];
+        }
+        Arrays.sort(diff);
+        int ans = 0;
+        for(int i=0; i<N-K; i++){
+            ans+=diff[i];
+        }
+        System.out.println(ans);
+
+    }
+}
