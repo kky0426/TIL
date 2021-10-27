6a3d8d42ede269c0aab4fa3e4fa8e483b922e099
kky0426 <ksakdlf99@naver.com>
2021-10-27 19:15:16 +0900

    commit

---
+++ BaekJoon/BOJ2666.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int ans = Integer.MAX_VALUE;
+    static int[] closet;
+    static int N,M;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int openA = Integer.parseInt(st.nextToken());
+        int openB = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        closet = new int[M];
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            closet[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(openA,openB,0,0);
+        System.out.println(ans);
+    }
+
+    static void dfs(int openA,int openB, int idx, int count){
+        if (idx == M){
+            ans = Math.min(ans,count);
+            return;
+        }
+        int moveA = Math.abs(openA-closet[idx]);
+        int moveB = Math.abs(openB-closet[idx]);
+
+        dfs(closet[idx],openB,idx+1,count+moveA);
+        dfs(openA,closet[idx],idx+1,count+moveB);
+    }
+
+
+}
