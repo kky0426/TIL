021de6805a90fd91c4d420badf32aee77b796e10
kky0426 <ksakdlf99@naver.com>
2022-03-15 22:40:19 +0900

    chore

--- BaekJoon/BOJ1477.py
+++ BaekJoon/BOJ1477.py
@@ -2,25 +2,24 @@ from sys import stdin
 
 N,M,L = map(int,stdin.readline().split())
 
-place = list(map(int,stdin.readline().split()))
-place.append(0)
-place.append(L-1)
-place.sort()
+spot = list(map(int,stdin.readline().split()))
+spot.append(0)
+spot.append(L)
+spot.sort()
 
-left=0
-right=L
+left = 1
+right = L
 ans = 0
 while left<=right:
-    mid = left+(right-left)//2
+    mid = (left+right)//2
     count = 0
     for i in range(1,N+2):
-        if place[i]-place[i-1]>mid:
-            count+=(place[i]-place[i-1]-1)//mid
-
+        if spot[i]-spot[i-1]>mid:
+            count+=(spot[i]-spot[i-1]-1)//mid
     if count<=M:
-        ans=mid
-        right=mid-1
+        ans = mid
+        right = mid-1
     else:
-        left=mid+1
+        left = mid+1
 
 print(ans)
